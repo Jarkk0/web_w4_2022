ad9eae3b9f41682c5af1bf893623a347cca9135a
Jarkk0 <uusijarkko@gmail.com>
2022-07-14 13:34:53 +0300

    add foodId

--- routes/recipe.js
+++ routes/recipe.js
@@ -2,16 +2,16 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-let recipes = [];
+// let recipes = [];
 
-fs.readFile('./data/recipe.json', "utf8", function(err, data) {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    recipes = JSON.parse(data);
-    console.log("data loaded")
-});
+// fs.readFile('./data/recipe.json', "utf8", function(err, data) {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     recipes = JSON.parse(data);
+//     console.log("data loaded")
+// });
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -19,8 +19,24 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:food',  (req, res, next) => {
-    res.json(recipes);
-    res.send('User Info ' + req.params.food)
+    //create JSON object
+    foodId = req.params.food;
+    let recipeJSON ={
+        "recipe": [
+          {
+            "name": foodId,
+            "instructions": "make pizza",
+            "ingredients": "salami, ananas"
+          }
+        ]
+    }
+    
+    res.send(recipeJSON);
+
+    //res.json(recipes);
+    //foodId = req.params.food;
+   // foodId.JSON = JSON.stringify(recipes);
+    //res.send();
 })
 
 module.exports = router;
